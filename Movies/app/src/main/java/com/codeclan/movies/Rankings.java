9333824fbda8190b390f190da7b7711d06005ae1
kimnorth <benignhumour@gmail.com>
2017-03-13 21:45:07 +0000

    Added a Ranking class and a RankingTest class.

---
+++ Movies/app/src/main/java/com/codeclan/movies/Rankings.java
@@ -0,0 +1,19 @@
+package com.codeclan.movies;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 13/03/2017.
+ */
+
+public class Rankings {
+
+    ArrayList<Movie> rankings;
+
+    public Rankings(){
+        this.rankings = new ArrayList<Movie>();
+    }
+
+    
+
+}
---
+++ Movies/app/src/test/java/com/codeclan/movies/RankingsTest.java
@@ -0,0 +1,9 @@
+package com.codeclan.movies;
+
+/**
+ * Created by user on 13/03/2017.
+ */
+
+public class RankingsTest {
+
+}
